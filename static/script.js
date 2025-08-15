171f692646ad762fb4a7edd7aa6a97e142975f42
Milleny Souza Braghine <Millenybragh@gmail.com>
2025-08-14 21:52:12 -0300

    Adição de slides e efeitos js

--- static/script.js
+++ static/script.js
@@ -1,17 +1,42 @@
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 200,
-    modifier: 1,
-    slideShadows: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+document.addEventListener("DOMContentLoaded", function() {
+    const sobreSection = document.getElementById("sobre");
+    const title = sobreSection.querySelector("h2");
+    const paragraph = sobreSection.querySelector("p");
+    const vanImage = document.getElementById("van");
+
+    function checkVisibility() {
+        
+        const sectionTop = sobreSection.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (sectionTop < windowHeight * 0.75) {
+            title.classList.add("visible");
+            paragraph.classList.add("visible");
+            vanImage.classList.add("visible");
+          
+            window.removeEventListener("scroll", checkVisibility);
+        }
+    }
+
+
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility();
+
+   
+    const slider = document.querySelector('.slider');
+    const images = document.querySelectorAll('.slider img');
+    let currentImageIndex = 0;
+
+    const nextSlide = () => {
+       
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        
+      
+        if (images[currentImageIndex]) {
+            slider.scrollLeft = images[currentImageIndex].offsetLeft;
+        }
+    };
+
+ 
+    setInterval(nextSlide, 3000);
 });
